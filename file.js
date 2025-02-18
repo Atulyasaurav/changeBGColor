90fa2a9a468ab76ecfedcc49351069055de89d4a
Atulya Saurav <atulyasaurav1@gmail.com>
2025-02-18 21:47:58 +0530

    update

--- file.js
+++ file.js
@@ -1,18 +1,7 @@
-const body = document.body;
-const btn = document.querySelector(".button");
-console.log(btn);
+// 
 
+console.log(1,2)
 
-const id = setInterval(() => {
-    const red = Math.floor(Math.random()*256);
-    const green = Math.floor(Math.random()*256);
-    const blue = Math.floor(Math.random()*256);
-    const rgb = `rgb(${red},${blue},${green})`;
-    body.style.background = rgb;
-    
-},000);
+arr = [1,2,3]
 
-btn.addEventListener("click",()=>{
-    clearInterval(id);
-    btn.textContent = body.style.background;
-});
+for(let i=0;i<)̥
